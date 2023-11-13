8891e732e0f9b061f1c84d7d454375c000eb45c0
zubair.osman <zubair.osman@gunsel.com.tr>
2023-11-13 18:15:38 +0200

    front

---
+++ frontend/src/models/note.ts
@@ -0,0 +1,27 @@
+export interface Note {
+    _id: string;
+    title: string;
+    text?: string;
+    folders?: string[];
+    tags?: string[];
+    categories?: string[];
+    createdAt: string;
+    updatedAt: string;
+}
+export interface FolderModel {
+    _id: string;
+    name: string;
+    // Other folder-related fields if needed
+}
+
+export interface TagModel {
+    _id: string;
+    name: string;
+    // Other tag-related fields if needed
+}
+
+export interface CategoryModel {
+    _id: string;
+    name: string;
+    // Other category-related fields if needed
+}
